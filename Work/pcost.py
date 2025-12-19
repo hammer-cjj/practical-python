3e8fae6b177a3526664db34056c6be1fd74e6e22
hammer-cjj <512823985@qq.com>
2025-12-19 21:08:57 +0800

    Completed Exercise 1.27: Reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
 # pcost.py
 #
 # Exercise 1.27
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    total = 0.0
+    for line in f:
+        line_list = line.split(',')
+        total += int(line_list[1]) * float(line_list[2][:-1])
+    print('Total cost', total)
