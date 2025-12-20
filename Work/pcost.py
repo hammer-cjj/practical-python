37ac27969e1ea2683bac184e82e728e6d8f4a51c
hammer-cjj <512823985@qq.com>
2025-12-20 21:08:08 +0800

    Exercise 1.30: Turning a script into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,24 @@
 # pcost.py
 #
 # Exercise 1.27
-with open('Data/portfolio.csv', 'rt') as f:
-    next(f)
+# with open('Data/portfolio.csv', 'rt') as f:
+#     next(f)
+#     total = 0.0
+#     for line in f:
+#         line_list = line.split(',')
+#         total += int(line_list[1]) * float(line_list[2][:-1])
+#     print('Total cost', total)
+
+# Exercise 1.30
+def portfolio_cost(filename):
     total = 0.0
-    for line in f:
-        line_list = line.split(',')
-        total += int(line_list[1]) * float(line_list[2][:-1])
-    print('Total cost', total)
+    with open('Data/portfolio.csv', 'rt') as f:
+        next(f)
+        for line in f:
+            line_list = line.split(',')
+            total += int(line_list[1]) * float(line_list[2][:-1])
+    return total
+
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
