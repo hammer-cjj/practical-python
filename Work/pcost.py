c61dcd2656d15364c9a18271f69461481d0b422d
hammer-cjj <512823985@qq.com>
2025-12-20 21:37:02 +0800

    Exercise 1.33: Reading from the command line

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,9 @@
 # pcost.py
-#
-# Exercise 1.27
-# with open('Data/portfolio.csv', 'rt') as f:
-#     next(f)
-#     total = 0.0
-#     for line in f:
-#         line_list = line.split(',')
-#         total += int(line_list[1]) * float(line_list[2][:-1])
-#     print('Total cost', total)
+import sys
 
-# Exercise 1.30
+# Exercise 1.33
+
+
 def portfolio_cost(filename):
     total = 0.0
     with open(filename, 'rt') as f:
@@ -24,5 +18,10 @@ def portfolio_cost(filename):
     return total
 
 
-cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
