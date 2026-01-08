a9105bb84f55e773692caf337c849fd5925ef004
hammer-cjj <512823985@qq.com>
2026-01-08 20:32:37 +0800

    Completed Exercise 5.7: Properties and Setters

--- Work/stock.py
+++ Work/stock.py
@@ -1,8 +1,18 @@
 class Stock:
     def __init__(self, name, shares, price):
         self.name = name
-        self.shares = shares
+        self._shares = shares
         self.price = price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Expected int")
+        self._shares = value
 
     def cost(self):
         return self.shares * self.price
