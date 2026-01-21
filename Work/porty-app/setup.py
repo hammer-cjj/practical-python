2477b40eceecc33badcae80baee58973a8e86e71
hammer-cjj <512823985@qq.com>
2026-01-21 21:04:22 +0800

    Completed Exercise 9.5: Make a package

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Cf",
+    author_email="cf@example.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+    scripts=["print-report.py"],
+)
